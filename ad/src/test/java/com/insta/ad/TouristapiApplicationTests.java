f36a5fdcce11df1d6138c2d5b872c1b60e566223
Kevin <irles.kevin@gmail.com>
2020-10-27 13:57:58 +0100

    ad

---
+++ ad/src/test/java/com/insta/ad/TouristapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insta.ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
